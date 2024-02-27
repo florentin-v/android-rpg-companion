cb8ac7c6e4e510f584c63addfb30cbd4ee6b1512
Florentin Vanaldewereld <160859560+florentin-v@users.noreply.github.com>
2024-02-27 12:49:03 +0100

    chore: fix detekt issues in mock-factory module

--- mock-factory/src/main/kotlin/com/fvanaldewereld/rpgcompanion/mockFactory/ScenarioDbObjectMockFactory.kt
+++ mock-factory/src/main/kotlin/com/fvanaldewereld/rpgcompanion/mockFactory/ScenarioDbObjectMockFactory.kt
@@ -1,3 +1,5 @@
+@file:Suppress("kotlin:S1192")
+
 package com.fvanaldewereld.rpgcompanion.mockFactory
 
 import com.fvanaldewereld.rpgcompanion.data.scenario.sources.localDatabase.embedded.Information
@@ -6,7 +8,6 @@ import com.fvanaldewereld.rpgcompanion.data.scenario.sources.localDatabase.entit
 import com.fvanaldewereld.rpgcompanion.data.scenario.sources.localDatabase.entities.Place
 import com.fvanaldewereld.rpgcompanion.data.scenario.sources.localDatabase.entities.ScenarioBase
 import com.fvanaldewereld.rpgcompanion.data.scenario.sources.localDatabase.relations.Scenario
-
 
 object ScenarioDbObjectMockFactory {
 
@@ -18,11 +19,20 @@ object ScenarioDbObjectMockFactory {
 
     private const val SCENARIO_AUTHOR = "Author"
 
-    private const val SCENARIO_SUMMARY_TEXT = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin malesuada condimentum. Aenean varius turpis a ornare lobortis. Curabitur massa turpis, commodo sit amet eleifend dignissim, porttitor ut dolor."""
+    private const val SCENARIO_SUMMARY_TEXT =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Sed sollicitudin malesuada condimentum. " +
+            "Aenean varius turpis a ornare lobortis. " +
+            "Curabitur massa turpis, commodo sit amet eleifend dignissim, porttitor ut dolor."
 
     private const val SCENARIO_CHAPTERS_INTRODUCTION = "Introduction"
 
-    private const val SCENARIO_CHAPTERS_INTRODUCTION_TEXT = """Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin aliquet at eros pretium porttitor. Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. Phasellus in commodo metus. Pellentesque lacinia orci et nisi ultricies, id tempus urna porta."""
+    private const val SCENARIO_CHAPTERS_INTRODUCTION_TEXT =
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. " +
+            "Proin aliquet at eros pretium porttitor. " +
+            "Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. " +
+            "Phasellus in commodo metus. " +
+            "Pellentesque lacinia orci et nisi ultricies, id tempus urna porta."
 
     private const val NUMBER_OF_PLAYERS_VALUE = 4
 
@@ -40,17 +50,69 @@ object ScenarioDbObjectMockFactory {
 
     private const val SCENARIO_CHARACTERS_NAME_2 = "Jane Doe"
 
-    private const val SCENARIO_CHARACTERS_DESCRIPTION_1 = """Nullam rutrum tincidunt neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Curabitur blandit turpis nibh, eu lacinia diam molestie ac. Vestibulum leo est, lobortis et tortor in, tempus egestas ligula. Mauris mollis vitae massa ac convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin aliquet at eros pretium porttitor. Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. Phasellus in commodo metus. Pellentesque lacinia orci et nisi ultricies, id tempus urna porta. Vestibulum dictum, enim euismod posuere interdum, massa quam blandit magna, in efficitur ligula lacus at augue. Nam hendrerit, quam ac rhoncus malesuada, diam justo accumsan ante, at gravida nulla arcu non mauris. Aenean volutpat faucibus laoreet. Maecenas dignissim et elit eu lobortis."""
+    private const val SCENARIO_CHARACTERS_DESCRIPTION_1 =
+        "Nullam rutrum tincidunt neque. " +
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, " +
+            "per inceptos himenaeos. " +
+            "Aliquam erat volutpat. " +
+            "Curabitur blandit turpis nibh, eu lacinia diam molestie ac. " +
+            "Vestibulum leo est, lobortis et tortor in, tempus egestas ligula. " +
+            "Mauris mollis vitae massa ac convallis. " +
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, " +
+            "per inceptos himenaeos. " +
+            "Proin aliquet at eros pretium porttitor. " +
+            "Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. " +
+            "Phasellus in commodo metus. " +
+            "Pellentesque lacinia orci et nisi ultricies, id tempus urna porta. " +
+            "Vestibulum dictum, enim euismod posuere interdum, " +
+            "massa quam blandit magna, in efficitur ligula lacus at augue. " +
+            "Nam hendrerit, quam ac rhoncus malesuada, " +
+            "diam justo accumsan ante, at gravida nulla arcu non mauris. " +
+            "Aenean volutpat faucibus laoreet. " +
+            "Maecenas dignissim et elit eu lobortis."
 
-    private const val SCENARIO_CHARACTERS_DESCRIPTION_2 = """Sed ullamcorper leo lacus, id mattis dolor efficitur ut. Quisque vestibulum sapien sit amet mi varius fermentum. Nullam nec quam rhoncus, collision dui ac, luctus nisi. Sed in mi eget enim porta vestibulum eu ut dui. Nulla faucibus quam ac elit semper varius. Duis luctus elit nec lorem ullamcorper, ut consequat ipsum tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor tincidunt nulla rutrum tincidunt ac id ligula. Nunc id accumsan sem, sed blandit lorem. Suspendisse mattis tellus ac nulla tincidunt condimentum. Vestibulum commodo nec sem id vestibulum. Donec commodo eget turpis at tincidunt. Donec ac magna suscipit, mollis arcu sed, euismod lectus."""
+    private const val SCENARIO_CHARACTERS_DESCRIPTION_2 =
+        "Sed ullamcorper leo lacus, id mattis dolor efficitur ut. " +
+            "Quisque vestibulum sapien sit amet mi varius fermentum. " +
+            "Nullam nec quam rhoncus, collision dui ac, luctus nisi. " +
+            "Sed in mi eget enim porta vestibulum eu ut dui. " +
+            "Nulla faucibus quam ac elit semper varius. " +
+            "Duis luctus elit nec lorem ullamcorper, ut consequat ipsum tristique. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Nulla vel tortor tincidunt nulla rutrum tincidunt ac id ligula. " +
+            "Nunc id accumsan sem, sed blandit lorem. " +
+            "Suspendisse mattis tellus ac nulla tincidunt condimentum. " +
+            "Vestibulum commodo nec sem id vestibulum. " +
+            "Donec commodo eget turpis at tincidunt. " +
+            "Donec ac magna suscipit, mollis arcu sed, euismod lectus."
 
     private const val SCENARIO_PLACES_NAME_1 = "Place 1"
 
     private const val SCENARIO_PLACES_NAME_2 = "Place 2"
 
-    private const val SCENARIO_PLACES_DESCRIPTION_1 = """Nullam sit amet dui non est auctor interdum eu ut velit. Etiam id diam erat. Sed ac mauris vitae arcu vulputate volutpat. Maecenas ut nulla vestibulum purus placerat rutrum. Quisque tellus ex, aliquet fermentum egestas aliquam, tincidunt eget sem. Praesent arcu felis, scelerisque vitae velit ornare, venenatis consectetur lacus. Curabitur consequat ipsum ac dapibus tincidunt. Pellentesque et sem mauris. Ut aliquam nulla non magna aliquam mattis. Aliquam a nulla bibendum, tempus velit et, vehicula elit."""
+    private const val SCENARIO_PLACES_DESCRIPTION_1 =
+        "Nullam sit amet dui non est auctor interdum eu ut velit. " +
+            "Etiam id diam erat. " +
+            "Sed ac mauris vitae arcu vulputate volutpat. " +
+            "Maecenas ut nulla vestibulum purus placerat rutrum. " +
+            "Quisque tellus ex, aliquet fermentum egestas aliquam, tincidunt eget sem. " +
+            "Praesent arcu felis, scelerisque vitae velit ornare, venenatis consectetur lacus. " +
+            "Curabitur consequat ipsum ac dapibus tincidunt. " +
+            "Pellentesque et sem mauris. " +
+            "Ut aliquam nulla non magna aliquam mattis. " +
+            "Aliquam a nulla bibendum, tempus velit et, vehicula elit."
 
-    private const val SCENARIO_PLACES_DESCRIPTION_2 = """Phasellus commodo enim scelerisque molestie varius. Aenean tincidunt aliquam mi a vestibulum. Aliquam imperdiet vel velit vitae blandit. Praesent a dignissim nunc. Aenean quis elementum enim, a vulputate augue. In eget aliquet elit. Ut nulla dui, eleifend eu urna sed, venenatis fermentum tellus. Mauris id ante ipsum. Vestibulum ac pulvinar ante. Morbi eget nisi erat."""
+    private const val SCENARIO_PLACES_DESCRIPTION_2 =
+        "Phasellus commodo enim scelerisque molestie varius. " +
+            "Aenean tincidunt aliquam mi a vestibulum. " +
+            "Aliquam imperdiet vel velit vitae blandit. " +
+            "Praesent a dignissim nunc. " +
+            "Aenean quis elementum enim, a vulputate augue. " +
+            "In eget aliquet elit. " +
+            "Ut nulla dui, eleifend eu urna sed, venenatis fermentum tellus. " +
+            "Mauris id ante ipsum. " +
+            "Vestibulum ac pulvinar ante. " +
+            "Morbi eget nisi erat."
 
     val information = Information(
         nbPlayers = NUMBER_OF_PLAYERS_VALUE,
@@ -107,5 +169,4 @@ object ScenarioDbObjectMockFactory {
             documentName = SCENARIO_DOCUMENT_NAME,
         ),
     )
-
 }
--- mock-factory/src/main/kotlin/com/fvanaldewereld/rpgcompanion/mockFactory/ScenarioModelMockFactory.kt
+++ mock-factory/src/main/kotlin/com/fvanaldewereld/rpgcompanion/mockFactory/ScenarioModelMockFactory.kt
@@ -1,3 +1,5 @@
+@file:Suppress("kotlin:S1192")
+
 package com.fvanaldewereld.rpgcompanion.mockFactory
 
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.AuthorModel
@@ -6,11 +8,13 @@ import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.ChaptersModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.CharacterModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.CharactersModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.DescriptionModel
+import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.GenresModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.InformationModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.PlaceModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.PlacesModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.ScenarioModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.SummaryModel
+import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.ThemesModel
 import com.fvanaldewereld.rpgcompanion.api.domain.scenario.models.TitleModel
 
 object ScenarioModelMockFactory {
@@ -23,11 +27,20 @@ object ScenarioModelMockFactory {
 
     private const val SCENARIO_AUTHOR = "Author"
 
-    private const val SCENARIO_SUMMARY_TEXT = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sollicitudin malesuada condimentum. Aenean varius turpis a ornare lobortis. Curabitur massa turpis, commodo sit amet eleifend dignissim, porttitor ut dolor."""
+    private const val SCENARIO_SUMMARY_TEXT =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Sed sollicitudin malesuada condimentum. " +
+            "Aenean varius turpis a ornare lobortis. " +
+            "Curabitur massa turpis, commodo sit amet eleifend dignissim, porttitor ut dolor."
 
     private const val SCENARIO_CHAPTERS_INTRODUCTION = "Introduction"
 
-    private const val SCENARIO_CHAPTERS_INTRODUCTION_TEXT = """Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin aliquet at eros pretium porttitor. Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. Phasellus in commodo metus. Pellentesque lacinia orci et nisi ultricies, id tempus urna porta."""
+    private const val SCENARIO_CHAPTERS_INTRODUCTION_TEXT =
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. " +
+            "Proin aliquet at eros pretium porttitor. " +
+            "Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. " +
+            "Phasellus in commodo metus. " +
+            "Pellentesque lacinia orci et nisi ultricies, id tempus urna porta."
 
     private const val NUMBER_OF_PLAYERS_VALUE = 4
 
@@ -45,25 +58,78 @@ object ScenarioModelMockFactory {
 
     private const val SCENARIO_CHARACTERS_NAME_2 = "Jane Doe"
 
-    private const val SCENARIO_CHARACTERS_DESCRIPTION_1 = """Nullam rutrum tincidunt neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Curabitur blandit turpis nibh, eu lacinia diam molestie ac. Vestibulum leo est, lobortis et tortor in, tempus egestas ligula. Mauris mollis vitae massa ac convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin aliquet at eros pretium porttitor. Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. Phasellus in commodo metus. Pellentesque lacinia orci et nisi ultricies, id tempus urna porta. Vestibulum dictum, enim euismod posuere interdum, massa quam blandit magna, in efficitur ligula lacus at augue. Nam hendrerit, quam ac rhoncus malesuada, diam justo accumsan ante, at gravida nulla arcu non mauris. Aenean volutpat faucibus laoreet. Maecenas dignissim et elit eu lobortis."""
+    private const val SCENARIO_CHARACTERS_DESCRIPTION_1 =
+        "Nullam rutrum tincidunt neque. " +
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. " +
+            "Aliquam erat volutpat. " +
+            "Curabitur blandit turpis nibh, eu lacinia diam molestie ac. " +
+            "Vestibulum leo est, lobortis et tortor in, tempus egestas ligula. " +
+            "Mauris mollis vitae massa ac convallis. " +
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. " +
+            "Proin aliquet at eros pretium porttitor. " +
+            "Vestibulum in elit dictum, faucibus lacus et, fermentum ipsum. " +
+            "Phasellus in commodo metus. " +
+            "Pellentesque lacinia orci et nisi ultricies, id tempus urna porta. " +
+            "Vestibulum dictum, enim euismod posuere interdum, " +
+            "massa quam blandit magna, in efficitur ligula lacus at augue. " +
+            "Nam hendrerit, quam ac rhoncus malesuada, diam justo accumsan ante, " +
+            "at gravida nulla arcu non mauris. " +
+            "Aenean volutpat faucibus laoreet. " +
+            "Maecenas dignissim et elit eu lobortis."
 
-    private const val SCENARIO_CHARACTERS_DESCRIPTION_2 = """Sed ullamcorper leo lacus, id mattis dolor efficitur ut. Quisque vestibulum sapien sit amet mi varius fermentum. Nullam nec quam rhoncus, collision dui ac, luctus nisi. Sed in mi eget enim porta vestibulum eu ut dui. Nulla faucibus quam ac elit semper varius. Duis luctus elit nec lorem ullamcorper, ut consequat ipsum tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor tincidunt nulla rutrum tincidunt ac id ligula. Nunc id accumsan sem, sed blandit lorem. Suspendisse mattis tellus ac nulla tincidunt condimentum. Vestibulum commodo nec sem id vestibulum. Donec commodo eget turpis at tincidunt. Donec ac magna suscipit, mollis arcu sed, euismod lectus."""
+    private const val SCENARIO_CHARACTERS_DESCRIPTION_2 =
+        "Sed ullamcorper leo lacus, id mattis dolor efficitur ut. " +
+            "Quisque vestibulum sapien sit amet mi varius fermentum. " +
+            "Nullam nec quam rhoncus, collision dui ac, luctus nisi. " +
+            "Sed in mi eget enim porta vestibulum eu ut dui. " +
+            "Nulla faucibus quam ac elit semper varius. " +
+            "Duis luctus elit nec lorem ullamcorper, ut consequat ipsum tristique. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Nulla vel tortor tincidunt nulla rutrum tincidunt ac id ligula. " +
+            "Nunc id accumsan sem, sed blandit lorem. " +
+            "Suspendisse mattis tellus ac nulla tincidunt condimentum. " +
+            "Vestibulum commodo nec sem id vestibulum. " +
+            "Donec commodo eget turpis at tincidunt. " +
+            "Donec ac magna suscipit, mollis arcu sed, euismod lectus."
 
     private const val SCENARIO_PLACES_NAME_1 = "Place 1"
 
     private const val SCENARIO_PLACES_NAME_2 = "Place 2"
 
-    private const val SCENARIO_PLACES_DESCRIPTION_1 = """Nullam sit amet dui non est auctor interdum eu ut velit. Etiam id diam erat. Sed ac mauris vitae arcu vulputate volutpat. Maecenas ut nulla vestibulum purus placerat rutrum. Quisque tellus ex, aliquet fermentum egestas aliquam, tincidunt eget sem. Praesent arcu felis, scelerisque vitae velit ornare, venenatis consectetur lacus. Curabitur consequat ipsum ac dapibus tincidunt. Pellentesque et sem mauris. Ut aliquam nulla non magna aliquam mattis. Aliquam a nulla bibendum, tempus velit et, vehicula elit."""
+    private const val SCENARIO_PLACES_DESCRIPTION_1 =
+        "Nullam sit amet dui non est auctor interdum eu ut velit. " +
+            "Etiam id diam erat. " +
+            "Sed ac mauris vitae arcu vulputate volutpat. " +
+            "Maecenas ut nulla vestibulum purus placerat rutrum. " +
+            "Quisque tellus ex, aliquet fermentum egestas aliquam, tincidunt eget sem. " +
+            "Praesent arcu felis, scelerisque vitae velit ornare, venenatis consectetur lacus. " +
+            "Curabitur consequat ipsum ac dapibus tincidunt. " +
+            "Pellentesque et sem mauris. " +
+            "Ut aliquam nulla non magna aliquam mattis. " +
+            "Aliquam a nulla bibendum, tempus velit et, vehicula elit."
 
-    private const val SCENARIO_PLACES_DESCRIPTION_2 = """Phasellus commodo enim scelerisque molestie varius. Aenean tincidunt aliquam mi a vestibulum. Aliquam imperdiet vel velit vitae blandit. Praesent a dignissim nunc. Aenean quis elementum enim, a vulputate augue. In eget aliquet elit. Ut nulla dui, eleifend eu urna sed, venenatis fermentum tellus. Mauris id ante ipsum. Vestibulum ac pulvinar ante. Morbi eget nisi erat."""
-
+    private const val SCENARIO_PLACES_DESCRIPTION_2 =
+        "Phasellus commodo enim scelerisque molestie varius. " +
+            "Aenean tincidunt aliquam mi a vestibulum. " +
+            "Aliquam imperdiet vel velit vitae blandit. " +
+            "Praesent a dignissim nunc. " +
+            "Aenean quis elementum enim, a vulputate augue. " +
+            "In eget aliquet elit. " +
+            "Ut nulla dui, eleifend eu urna sed, venenatis fermentum tellus. " +
+            "Mauris id ante ipsum. " +
+            "Vestibulum ac pulvinar ante. " +
+            "Morbi eget nisi erat."
 
     val titleModel = TitleModel(SCENARIO_TITLE)
     val authorModel = AuthorModel(SCENARIO_AUTHOR)
     val informationModel = InformationModel(
         nbPlayers = NUMBER_OF_PLAYERS_VALUE,
-        genres = listOf(GENRES_HORROR, GENRES_DRAMA),
-        themes = listOf(THEMES_ALIEN, THEMES_POSSESSION, THEMES_PSYCOLOGICAL),
+        genres = GenresModel(
+            genres = listOf(GENRES_HORROR, GENRES_DRAMA),
+        ),
+        themes = ThemesModel(
+            themes = listOf(THEMES_ALIEN, THEMES_POSSESSION, THEMES_PSYCOLOGICAL),
+        ),
     )
     val summaryDescriptionModel = DescriptionModel(
         paragraphs = listOf(SCENARIO_SUMMARY_TEXT),
@@ -159,5 +225,4 @@ object ScenarioModelMockFactory {
     val exampleDescriptionModel = DescriptionModel(
         paragraphs = listOf("example"),
     )
-
 }
