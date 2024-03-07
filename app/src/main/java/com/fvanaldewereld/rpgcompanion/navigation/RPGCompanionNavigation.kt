0d0743cfee94e37d630f2d0cc0da7281e7e9966d
Baptiste Carlier <4005409+BapNesS@users.noreply.github.com>
2024-03-08 00:35:29 +0100

    chore: Improve ViewModel default parameter (#43)
    * chore: Improve ViewModel default parameter
    
    * chore: Remove unused import

--- app/src/main/java/com/fvanaldewereld/rpgcompanion/navigation/RPGCompanionNavigation.kt
+++ app/src/main/java/com/fvanaldewereld/rpgcompanion/navigation/RPGCompanionNavigation.kt
@@ -6,7 +6,6 @@ import androidx.navigation.compose.rememberNavController
 import com.fvanaldewereld.rpgcompanion.ui.components.home.HomeScreen
 import com.fvanaldewereld.rpgcompanion.ui.scenario.detail.components.ScenarioDetailScreen
 import com.fvanaldewereld.rpgcompanion.ui.scenario.list.components.ScenarioListScreen
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
 internal fun RPGCompanionNavigation() {
@@ -22,14 +21,6 @@ internal fun RPGCompanionNavigation() {
         // This method navigates to a specified route (String).
         fun navigateTo(navRoute: String) = navHostController.navigate(navRoute)
 
-        // This method navigates to a route while popping all destinations up to a specified route
-        fun navigatePopAllTo(navRoute: NavigationRoute, navRouteToPopUpTo: NavigationRoute) {
-            navHostController.navigate(navRoute.route) {
-                // Configure popping behavior
-                popUpTo(navRouteToPopUpTo.route) { inclusive = true }
-            }
-        }
-
         animatedComposable(route = NavigationRoute.Home.route) {
             HomeScreen {
                 navigateTo(NavigationRoute.ScenarioList)
@@ -38,21 +29,15 @@ internal fun RPGCompanionNavigation() {
 
         animatedComposable(route = NavigationRoute.ScenarioList.route) {
             ScenarioListScreen(
-                viewModel = koinViewModel(),
                 onBackButtonPressed = ::navigateBack,
                 goToScenarioDetail = { scenarioId ->
-                    navigateTo(
-                        NavigationRoute.ScenarioDetail.createRoute(scenarioId = scenarioId),
-                    )
+                    navigateTo(NavigationRoute.ScenarioDetail.createRoute(scenarioId = scenarioId))
                 },
             )
         }
 
         animatedComposable(route = NavigationRoute.ScenarioDetail.route) {
-            ScenarioDetailScreen(
-                viewModel = koinViewModel(),
-                onBackButtonPressed = ::navigateBack,
-            )
+            ScenarioDetailScreen(onBackButtonPressed = ::navigateBack)
         }
     }
 }
